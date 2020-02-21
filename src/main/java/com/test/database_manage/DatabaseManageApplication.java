acaf96a2bdba4ace66526ef0a2bffa6f11b27c0a
OutcastBoy <q1447132952Q>
2020-02-21 12:43:27 +0800

    初始化项目

---
+++ src/main/java/com/test/database_manage/DatabaseManageApplication.java
@@ -0,0 +1,13 @@
+package com.test.database_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseManageApplication.class, args);
+    }
+
+}
